c5fe88a3b987466314f8e747afb83efaabf0eb91
Salmanabdullah <salmandcc@gmail.com>
2022-02-26 23:29:52 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Topbar from "./components/navbar/nav-bar";
+
+function App() {
+  return (
+    <Topbar/>
+  );
+}
+
+export default App;
