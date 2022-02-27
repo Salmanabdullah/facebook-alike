8715cdb55cd5eccecde155b7c82a5d8698796011
Salmanabdullah <salmandcc@gmail.com>
2022-02-27 22:18:36 +0100

    Create rightsidebar.js

---
+++ src/components/right-sidebar/rightsidebar.js
@@ -0,0 +1,66 @@
+import "./rightsidebar.css";
+
+function RightSideBar() {
+  return (
+    <section className="float-end rightSideBar">
+      <div>
+        <img src="https://via.placeholder.com/30" alt="" />
+        <span>user</span>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/30" alt="" />
+        <span>friends</span>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/30" alt="" />
+        <span>groups</span>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/30" alt="" />
+        <span>marketplace</span>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/30" alt="" />
+        <span>watch</span>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/30" alt="" />
+        <span>memories</span>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/30" alt="" />
+        <span>fjdsafj</span>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/30" alt="" />
+        <span>fjdsafj</span>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/30" alt="" />
+        <span>fjdsafj</span>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/30" alt="" />
+        <span>fjdsafj</span>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/30" alt="" />
+        <span>fjdsafj</span>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/30" alt="" />
+        <span>fjdsafj</span>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/30" alt="" />
+        <span>fjdsafj</span>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/30" alt="" />
+        <span>fjdsafj</span>
+      </div>
+    </section>
+  );
+}
+
+export default RightSideBar;
