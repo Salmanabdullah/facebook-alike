9ae2fd9f2addb14158d97652aadb04d9b32c6fb1
Salmanabdullah <salmandcc@gmail.com>
2022-02-27 22:18:28 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Topbar from "./components/navbar/nav-bar";
+import LeftSideBar from "./components/left-sidebar/leftsidebar";
+import RightSideBar from "./components/right-sidebar/rightsidebar";
 
 function App() {
   return (
-    <Topbar/>
+    <>
+      <Topbar />
+      <div>
+        <div className="row">
+          <div className="col">
+            <LeftSideBar />
+          </div>
+          <div className="col-6">ksdfdskf</div>
+          <div className="col">
+            <RightSideBar />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
