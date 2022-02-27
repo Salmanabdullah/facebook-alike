f46526fbb54095a46be31f785fc4623fba284a73
Salmanabdullah <salmandcc@gmail.com>
2022-02-27 22:18:31 +0100

    Create leftsidebar.js

---
+++ src/components/left-sidebar/leftsidebar.js
@@ -0,0 +1,66 @@
+import "./leftsidebar.css";
+
+function LeftSideBar() {
+  return (
+    <section className="leftsidebar">
+      <div className="border">
+        <img src="https://via.placeholder.com/30" alt="" />
+        <span>user</span>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/30" alt="" />
+        <span>friends</span>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/30" alt="" />
+        <span>groups</span>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/30" alt="" />
+        <span>marketplace</span>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/30" alt="" />
+        <span>watch</span>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/30" alt="" />
+        <span>memories</span>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/30" alt="" />
+        <span>fjdsafj</span>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/30" alt="" />
+        <span>fjdsafj</span>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/30" alt="" />
+        <span>fjdsafj</span>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/30" alt="" />
+        <span>fjdsafj</span>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/30" alt="" />
+        <span>fjdsafj</span>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/30" alt="" />
+        <span>fjdsafj</span>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/30" alt="" />
+        <span>fjdsafj</span>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/30" alt="" />
+        <span>fjdsafj</span>
+      </div>
+    </section>
+  );
+}
+
+export default LeftSideBar;
