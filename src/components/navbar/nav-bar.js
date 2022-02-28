d5823a45969da9cf5e1ffe213a1b247fb20daedf
Salmanabdullah <salmandcc@gmail.com>
2022-02-28 17:26:11 +0100

    Update nav-bar.js

--- src/components/navbar/nav-bar.js
+++ src/components/navbar/nav-bar.js
@@ -4,18 +4,24 @@ import "./nav-bar.css";
 function Topbar() {
   return (
     <section className="top-bar d-flex justify-content-between">
-      <article className="mid-icons d-flex justify-content-start gap-3 ms-0">
-        <div>logo</div>
+      <article className="left-icons d-flex justify-content-start gap-3 ms-0">
+        <span>logo</span>
         <div>search</div>
       </article>
       <article className="mid-icons d-flex justify-content-center gap-3">
+        {/* <tr>
+          <td>home</td>
+          <td>homeless</td>
+          <td>home</td>
+          <td>home</td>
+        </tr> */}
         <div>home</div>
         <div>watch</div>
         <div>marketplace</div>
         <div>groups</div>
         <div>gaming</div>
       </article>
-      <article className="mid-icons d-flex justify-content-end gap-3 me-0">
+      <article className="right-icons d-flex justify-content-end gap-3 me-0">
         <div>profile</div>
         <div>menu</div>
         <div>messenger</div>
